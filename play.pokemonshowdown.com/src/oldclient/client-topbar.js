e93a8eeaead49605ef408ef3a56c63b7da75e1c2
Ag3nt-9 <85025522+Ag3nt-9@users.noreply.github.com>
2026-07-23 23:02:02 -0500

    Update client-topbar.js

--- play.pokemonshowdown.com/src/oldclient/client-topbar.js
+++ play.pokemonshowdown.com/src/oldclient/client-topbar.js
@@ -754,7 +754,7 @@
 			buf += '<p>Choose an avatar or <button name="close" class="button">Cancel</button></p>';
 
 			buf += '<div class="avatarlist">';
-			for (var i = 1; i <= 5; i++) {
+			for (var i = 1; i <= 6; i++) {
 				var offset = '-' + (((i - 1) % 16) * 80 + 1) + 'px -' + (Math.floor((i - 1) / 16) * 80 + 1) + 'px';
 				buf += '<button name="setAvatar" value="' + i + '" style="background-position:' + offset + '" class="option pixelated' + (i === cur ? ' cur' : '') + '" title="/avatar ' + i + '"></button>';
 			}
